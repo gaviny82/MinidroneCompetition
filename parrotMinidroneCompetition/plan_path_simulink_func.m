c5d6edb422e5a11a4c677433ec4147ebc7642ce9
JinchengYou <jincheng.you@outlook.com>
2022-08-04 00:10:01 +0800

    Add stateflow logic to control subsystem

---
+++ parrotMinidroneCompetition/plan_path_simulink_func.m
@@ -0,0 +1,11 @@
+function [landing_zone_detected, takeoff_complete, target_x, target_y] = plan_path_simulink_func(current_pos, vision_data)
+%PLAN_PATH_SIMULINK_FUNC Provides navigation point in traking stage and determines if the landing zone is detected
+%   Detailed explanation goes here
+
+    landing_zone_detected = 0;
+    takeoff_complete = 0;
+    target_x = 0;
+    target_y = 1;
+
+end
+
