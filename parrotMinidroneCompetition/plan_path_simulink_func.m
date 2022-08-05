d61c1c48d6b65ec11e4fc97b4e5c318b6eaa800f
JinchengYou <jincheng.you@outlook.com>
2022-08-05 18:17:26 +0800

    Add data store

--- parrotMinidroneCompetition/plan_path_simulink_func.m
+++ parrotMinidroneCompetition/plan_path_simulink_func.m
@@ -1,11 +1,25 @@
-function [landing_zone_detected, takeoff_complete, target_x, target_y] = plan_path_simulink_func(current_pos, vision_data)
+function [takeoff_complete, landing_ready, landing_zone_detected, target_x, target_y, target_z, motion_history] = plan_path_simulink_func(current_pos, vision_data, motion_history)
 %PLAN_PATH_SIMULINK_FUNC Provides navigation point in traking stage and determines if the landing zone is detected
 %   Detailed explanation goes here
 
-    landing_zone_detected = 0;
+    % Record current motion
+    x_history = motion_history(1,:);
+    y_history = motion_history(2,:);
+    z_history = motion_history(3,:);
+
+    x_hitory = [current_pos.X, x_history(1:end - 1)];
+    y_hitory = [current_pos.Y, y_history(1:end - 1)];
+    z_hitory = [current_pos.Z, z_history(1:end - 1)];
+
+    motion_history = [x_history;y_history;z_history];
+
+    % TODO: process vision data
     takeoff_complete = 0;
+    landing_ready = 0;
+    landing_zone_detected = 0;
     target_x = 0;
     target_y = 1;
+    target_z = 0;
 
 end
 
