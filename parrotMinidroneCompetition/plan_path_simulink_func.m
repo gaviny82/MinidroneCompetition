597991847fcd9e87aa922892d278f95804662c62
JinchengYou <jincheng.you@outlook.com>
2022-08-06 23:46:02 +0800

    Fix data types

--- parrotMinidroneCompetition/plan_path_simulink_func.m
+++ parrotMinidroneCompetition/plan_path_simulink_func.m
@@ -1,22 +1,21 @@
 function [takeoff_complete, landing_ready, landing_zone_detected, target_x, target_y, target_z, motion_history] = plan_path_simulink_func(current_pos, vision_data, motion_history)
 %PLAN_PATH_SIMULINK_FUNC Provides navigation point in traking stage and determines if the landing zone is detected
-%   Detailed explanation goes here
 
     % Record current motion
     x_history = motion_history(1,:);
     y_history = motion_history(2,:);
     z_history = motion_history(3,:);
 
-    x_hitory = [current_pos.X, x_history(1:end - 1)];
-    y_hitory = [current_pos.Y, y_history(1:end - 1)];
-    z_hitory = [current_pos.Z, z_history(1:end - 1)];
+    x_history = [current_pos.X, x_history(1:end - 1)];
+    y_history = [current_pos.Y, y_history(1:end - 1)];
+    z_history = [current_pos.Z, z_history(1:end - 1)];
 
-    motion_history = [x_history;y_history;z_history];
+    motion_history = cast([x_history;y_history;z_history], "double"); % Set data type for code generation
 
     % TODO: process vision data
-    takeoff_complete = 0;
-    landing_ready = 0;
-    landing_zone_detected = 0;
+    takeoff_complete = false;
+    landing_ready = false;
+    landing_zone_detected = false;
     target_x = 0;
     target_y = 1;
     target_z = 0;
