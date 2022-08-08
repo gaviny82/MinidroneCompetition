b3ba7b22272ac8b5f0b473c7a50e9d1b5ac9023d
JinchengYou <jincheng.you@outlook.com>
2022-08-08 23:35:01 +0800

    Add current_state as an input to plan_path function

--- parrotMinidroneCompetition/plan_path_simulink_func.m
+++ parrotMinidroneCompetition/plan_path_simulink_func.m
@@ -1,5 +1,13 @@
-function [takeoff_complete, landing_ready, landing_zone_detected, target_x, target_y, target_z, motion_history] = plan_path_simulink_func(current_pos, vision_data, motion_history)
+function [takeoff_complete, landing_ready, landing_zone_detected, target_x, target_y, target_z, motion_history] = plan_path_simulink_func(current_pos, vision_data, current_state, motion_history)
 %PLAN_PATH_SIMULINK_FUNC Provides navigation point in traking stage and determines if the landing zone is detected
+%
+%   @param current_pos: structure that contains the current position, orientation, velocity and angular velocity of the minidrone.
+%   @param vision_data: an array of breakpoint coordinates (pixel)
+%   @param current_state:   enum value which represents the current state of the state machine.
+%                           0: TakeOff, 1: Tracking, 2: ApproachingLandingSite, 3: Descending
+%   @param motion_history: a 3x100 single array that store the positions in the past 0.5 second.
+%
+
 
     % Record current motion
     x_history = motion_history(1,:);
@@ -13,6 +21,9 @@ function [takeoff_complete, landing_ready, landing_zone_detected, target_x, targ
     motion_history = cast([x_history;y_history;z_history], "double"); % Set data type for code generation
 
     % TODO: process vision data
+
+    % TODO: convert pixel distance to metres using Pythagorus
+    
     takeoff_complete = false;
     landing_ready = false;
     landing_zone_detected = false;
